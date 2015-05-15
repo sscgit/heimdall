83087102d82a0727f845d61173fbe5b75bb95a2f
Sören Schreiber <github@schreiber.io>
2015-05-15 16:38:19 +0200

    added local modifications, wip

--- config.php
+++ config.php
@@ -11,19 +11,24 @@
  * define ip address and port here
  */
 $source = $_SERVER['SERVER_ADDR'];
-$target = shell_exec("hostname -I");
+$target = shell_exec("hostname -I | awk '{print $1}'");
+//$target = '192.168.1.41';
 $port = 11337;
 
 /*
  * specify configuration of sockets to use
- *   array("group", "plug", "description");
+ *	array("group", "plug", "description");
  * use empty string to create empty box
  *   ""
  *
  */
 $config=array(
-  array("00010", "02", "LED Streifen"),
-  array("00010", "01", "5.1 Anlage"),
-  array("00010", "03", "Bettlampe"),
+	array("01001", "01", "Wohnzimmer Mitte"),
+	array("01001", "02", "Wohnzimmer Ecke"),
+	array("01010", "01", "Flur"),
+	array("01100", "01", "Bibliothek Ecke"),
+	array("01100", "02", "Bibliothek Schreibtisch"),
+	array("01011", "01", "Schlafzimmer (West)"),
+	array("01011", "02", "Schlafzimmer (Ost)"),
 )
 ?>
--- remote.php
+++ remote.php
@@ -1,90 +1,107 @@
 <?php
 include("config.php");
 
-if (isset($_GET['group'])) $nGroup=$_GET['group'];
-else $nGroup="";
-if (isset($_GET['switch'])) $nSwitch=$_GET['switch'];
-else $nSwitch="";
-if (isset($_GET['action'])) $nAction=$_GET['action'];
-else $nAction="";
-if (isset($_GET['delay'])) $nDelay=$_GET['delay'];
-else $nDelay=0;
+if (isset($_GET['group'])) {
+	$nGroup=$_GET['group'];
+} else { 
+	$nGroup=""; 
+}
+
+if (isset($_GET['switch'])) {
+	$nSwitch=$_GET['switch'];
+} else {
+	$nSwitch="";
+}
+
+if (isset($_GET['action'])) {
+	$nAction=$_GET['action'];
+} else {
+	$nAction="";
+}
+
+if (isset($_GET['delay'])) {
+	$nDelay=$_GET['delay'];
+} else {
+	$nDelay=0;
+}
 
 $output = $nGroup.$nSwitch.$nAction.$nDelay;
 if (strlen($output) >= 8) {
-  $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
-  socket_bind($socket, $source) or die("Could not bind to socket\n");
-  socket_connect($socket, $target, $port) or die("Could not connect to socket\n");
-  socket_write($socket, $output, strlen ($output)) or die("Could not write output\n");
-  socket_close($socket);
-  header("Location: index.php");
+	$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
+	socket_bind($socket, $source) or die("Could not bind to socket\n");
+	socket_connect($socket, $target, $port) or die("Could not connect to socket\n");
+	socket_write($socket, $output, strlen ($output)) or die("Could not write output\n");
+	socket_close($socket);
+	header("Location: remote.php");
+	//header("Location: index.php");
 }
 ?>
 <html>
-  <head>
-    <title>SmartControl</title>
-    <link rel="stylesheet" href="resources/style.css">
-<!--[if IE]>
-	<link rel="stylesheet" type="text/css" href="ie.css" />
-<![endif]-->
-	<link rel="icon"
-          type="image/png"
-          href="resources/favicon.ico">
-	<meta charset="UTF-8">
-	<meta name="mobile-web-app-capable" content="yes">
-	<meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="viewport"
-          content="
-              height = device-height,
-              width = device-width,
-              initial-scale = 1.0,
-              user-scalable = no ,
-              target-densitydpi = device-dpi
-              " />
-  </head>
+	<head>
+		<title>Heimdall</title>
+		<link rel="stylesheet" href="resources/style.css">
+		<!--[if IE]>
+			<link rel="stylesheet" type="text/css" href="ie.css" />
+		<![endif]-->
+		<link rel="icon" type="image/png" href="resources/favicon.ico">
+		<meta charset="UTF-8">
+		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="viewport" content=" height = device-height, width = device-width, initial-scale = 1.0, user-scalable = no, target-densitydpi = device-dpi" />
+	</head>
 <body>
-<div id="header">
-SmartControl
-</div>
+	<div id="header">
+		Heimdall
+	</div>
+	<div id="navigation">
+		<ul>
+			<li><a href="remote.php">Devices</a></li>
+			<li><a href="">Thermo</a></li>
+			<li><a href="">Access</a></li>
+			<li><a href="">Settings</a></li>
+		</ul>
+	</div>
+	<div style="clear:both;"></div>
 <?php
 $index=0;
 
 foreach($config as $current) {
-  if ($current != "") {
-    $ig = $current[0];
-    $is = $current[1];
-    $id = $current[2];
-	
-    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
-    socket_bind($socket, $source) or die("Could not bind to socket\n");
-    socket_connect($socket, $target, $port) or die("Could not connect to socket\n");
+	if ($current != "") {
+		$ig = $current[0];
+		$is = $current[1];
+		$id = $current[2];
+      
+		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
+		socket_bind($socket, $source) or die("Could not bind to socket\n");
+		socket_connect($socket, $target, $port) or die("Could not connect to socket\n");
+		
+		$output = $ig.$is."2";
+		socket_write($socket, $output, strlen ($output)) or die("Could not write output\n");
+		$state = socket_read($socket, 2048);
 
-    $output = $ig.$is."2";
-    socket_write($socket, $output, strlen ($output)) or die("Could not write output\n");
-    $state = socket_read($socket, 2048);
+		if ($state == 0) {
+			$ia = 1;
+			$direction="on";
+			error_log("on");
+		}
+		if ($state == 1) {
+			$ia = 0;
+			$direction="off";
+			error_log("off");
+		}
+		echo "<a href=\"?group=".$ig;
+		echo "&switch=".$is;
+		echo "&action=".$ia."\">";
+		echo "<div class=\"entry state" . $state . "\">";
+		echo "<div class=\"switch\"></div>";
+		echo "<span class=\"info\">".$id."</span>";
+		echo "<span class=\"channel\">Kanal ".$ig.":".$is."</span>";
+		echo "</div>\n";
+		echo "</a>\n";
 
-    if ($state == 0) {
-      $ia = 1;
-      $direction="on";
-    }
-    if ($state == 1) {
-      $ia = 0;
-      $direction="off";
-    }
-    echo "<a href=\"?group=".$ig;
-    echo "&switch=".$is;
-    echo "&action=".$ia."\">";
-	echo "<div class=\"entry state" . $state . "\">";
-	echo "<div class=\"switch\"></div>";
-    echo "<span class=\"info\">".$id."</span>";
-    echo "<span class=\"channel\">Kanal ".$ig.":".$is."</span>";
-    echo "</div>\n";
-    echo "</a>\n";
-
-  }
-  $index++;
-
+	}
+	$index++;
 }
 ?>
-</body>
+	</body>
 </html>
